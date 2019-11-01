ee1e9d7c28cebf2f8f5d88ad969363323ac246ab
Raul Garcia <garciacalvoraul@gmail.com>
2019-11-01 17:49:06 +0000

    Partially create SuggestionStage

---
+++ jsymspell/src/main/java/rgc/SuggestionStage.java
@@ -0,0 +1,85 @@
+package rgc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SuggestionStage {
+
+  private class Node {
+    private final String suggestion;
+    private final int next;
+
+    public Node(String suggestion, int next) {
+      this.suggestion = suggestion;
+      this.next = next;
+    }
+  }
+
+  private class Entry {
+    private int count;
+    private int first;
+
+    private Entry(int count, int first) {
+      this.count = count;
+      this.first = first;
+    }
+
+    private int incrCount() {
+      this.count++;
+      return count;
+    }
+
+    public void setFirst(int first) {
+      this.first = first;
+    }
+  }
+
+  private final Map<Integer, Entry> deletes;
+  private final ChunkArray<Node> nodes;
+
+  public SuggestionStage(int initialCapacity) {
+    this.deletes = new HashMap<>(initialCapacity);
+    this.nodes = new ChunkArray<>(Node.class, initialCapacity * 2);
+  }
+
+  public int deleteCount() {
+    return deletes.size();
+  }
+
+  public int nodeCount() {
+    return nodes.getCount();
+  }
+
+  private void add(int deleteHash, String suggestion) {
+    Entry entry = deletes.getOrDefault(deleteHash, new Entry(0, -1));
+    int next = entry.first;
+    entry.incrCount();
+    entry.setFirst(nodes.getCount());
+    deletes.put(deleteHash, entry);
+    nodes.add(new Node(suggestion, next));
+  }
+
+  private void commitTo(Map<Integer, String[]> permanentDeletes) {
+    for (Map.Entry<Integer, Entry> entry : deletes.entrySet()) {
+      int i = 0;
+      String[] suggestions = permanentDeletes.get(entry.getKey());
+      if (suggestions != null) {
+        i = suggestions.length;
+        var newSuggestions = new String[suggestions.length + entry.getValue().count];
+        System.arraycopy(suggestions, 0, newSuggestions, 0, suggestions.length);
+        permanentDeletes.put(entry.getKey(), newSuggestions);
+      } else {
+        suggestions = new String[entry.getValue().count];
+        permanentDeletes.put(entry.getKey(), suggestions);
+      }
+
+      int next = entry.getValue().first;
+      while (next >= 0) {
+        var node = nodes.get(next);
+        suggestions[i] = node.suggestion;
+        next = node.next;
+        i++;
+      }
+    }
+  }
+}
