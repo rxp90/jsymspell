0d8aef61e29250f0c2b751662b45c133bfd198df
Raul Garcia <garciacalvoraul@gmail.com>
2019-11-15 08:56:39 +0000

    Add tests for SuggestItem

---
+++ jsymspell/jsymspell-core/src/test/java/io/gitlab/rxp90/jsymspell/SuggestItemTest.java
@@ -0,0 +1,34 @@
+package io.gitlab.rxp90.jsymspell;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SuggestItemTest {
+
+  @Test
+  void compareToAscendingByDistance() {
+    SuggestItem smallDistance = new SuggestItem("test", 5, 10);
+    SuggestItem bigDistance = new SuggestItem("test", 10, 10);
+
+    assertEquals(-1, smallDistance.compareTo(bigDistance));
+  }
+
+  @Test
+  void compareToDescendingByFrequency() {
+    SuggestItem sameDistanceSmallerFreq = new SuggestItem("test", 5, 10);
+    SuggestItem sameDistanceBiggerFreq = new SuggestItem("test", 5, 20);
+
+    assertEquals(1, sameDistanceSmallerFreq.compareTo(sameDistanceBiggerFreq));
+  }
+
+
+  @Test
+  void equalsAndHashCode() {
+    SuggestItem si1 = new SuggestItem("test", 5, 10);
+    SuggestItem si2 = new SuggestItem("test", 5, 10);
+
+    assertEquals(si1, si2);
+    assertEquals(si1.hashCode(), si2.hashCode());
+  }
+}
