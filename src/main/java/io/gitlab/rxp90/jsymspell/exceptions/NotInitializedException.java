84a6652ffbe62559eda11c6886cd93170d4bb421
rgarcia <garciacalvoraul@gmail.com>
2020-11-03 11:43:37 +0000

    Make NotInitializedException extend from JSymSpellException

--- src/main/java/io/gitlab/rxp90/jsymspell/exceptions/NotInitializedException.java
+++ src/main/java/io/gitlab/rxp90/jsymspell/exceptions/NotInitializedException.java
@@ -1,7 +1,6 @@
 package io.gitlab.rxp90.jsymspell.exceptions;
 
-public class NotInitializedException extends Exception {
-
+public class NotInitializedException extends JSymSpellException {
     public NotInitializedException(String message) {
         super(message);
     }
